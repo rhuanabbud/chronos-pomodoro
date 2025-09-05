16d05bfff81f97f00592e966280e549d4e11351e
Rhuan Abbud <rhuan.abbud@hotmail.com>
2025-09-05 14:52:14 -0300

    css globais e em escopo de components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1>Olá, mundo! (do APP)</h1>
+      <Heading />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime adipisci
         libero, itaque, molestias impedit tenetur cumque necessitatibus vitae
